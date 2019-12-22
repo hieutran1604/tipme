173dc34580b5726271d8af10f4b58e7373737c7c
PJay Tran <pjaytran@PJays-iMac.local>
2019-12-21 23:54:25 -0800

    Initial Commit

---
+++ tipme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipme
+//
+//  Created by PJay Tran on 12/21/19.
+//  Copyright © 2019 sjsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
